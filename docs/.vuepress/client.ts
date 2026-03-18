fde392a8b8c550a8385630d6c8b96c4d6a3ddbe9
linuxarsenal <tgdzsjh@163.com>
2026-03-18 22:32:31 +0800

    fix: disable custom component for testing

--- docs/.vuepress/client.ts
+++ docs/.vuepress/client.ts
@@ -1,6 +1,5 @@
 import { defineClientConfig } from 'vuepress/client'
-import RightActionBar from './components/RightActionBar.vue'
 
 export default defineClientConfig({
-  rootComponents: [RightActionBar],
+  rootComponents: [],
 })
